a86e74d1af8bd592aabe7292982b0f85cff75257
vandasavkina <wandasavkina@hotmail.it>
2024-03-22 15:02:54 +0100

    Initial commit

---
+++ app/vanda/page.tsx
@@ -0,0 +1,8 @@
+export default async function VandaPage() {
+ return         <h1
+ className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
+ style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+>
+Vanda
+</h1>
+}
