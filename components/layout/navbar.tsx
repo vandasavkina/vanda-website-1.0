9f1966b3fca6d876d409a7bdbfeadf0504acbabf
vandasavkina <wandasavkina@hotmail.it>
2024-03-22 15:18:08 +0100

    force build again

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -7,13 +7,11 @@ import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 import { Session } from "next-auth";
 
-export default function NavBar({ session }: { session: Session | null }) {
-  const { SignInModal, setShowSignInModal } = useSignInModal();
+export default function NavBar() {
   const scrolled = useScroll(50);
 
   return (
     <>
-      <SignInModal />
       <div
         className={`fixed top-0 w-full flex justify-center ${
           scrolled
@@ -33,16 +31,11 @@ export default function NavBar({ session }: { session: Session | null }) {
             <p>Precedent</p>
           </Link>
           <div>
-            {session ? (
-              <UserDropdown session={session} />
-            ) : (
               <button
                 className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                onClick={() => setShowSignInModal(true)}
               >
                 Sign In
               </button>
-            )}
           </div>
         </div>
       </div>
