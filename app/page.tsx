2e51e5bcde902ab058cb5cf26f8c215660aa93b9
vandasavkina <wandasavkina@hotmail.it>
2024-03-22 15:30:29 +0100

    updated twitter link & changed title

--- app/page.tsx
+++ app/page.tsx
@@ -27,21 +27,21 @@ export default async function Home() {
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
         <a
-          href="/vanda"
+          href="https://twitter.com/vandasavkina"
           target="_blank"
           rel="noreferrer"
           className="mx-auto mb-5 flex max-w-fit animate-fade-up items-center justify-center space-x-2 overflow-hidden rounded-full bg-blue-100 px-7 py-2 transition-colors hover:bg-blue-200"
         >
           <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing Precedent
+            Go to Vanda Twitter
           </p>
         </a>
         <h1
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          Building blocks for your Next project
+Hello I AM Vanda
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
@@ -91,7 +91,7 @@ export default async function Home() {
         </div>
       </div>
       <div className="my-10 grid w-full max-w-screen-xl animate-fade-up grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0">
-        {features.map(({ title, description, demo, large }) => (
+        {/* {features.map(({ title, description, demo, large }) => (
           <Card
             key={title}
             title={title}
@@ -105,7 +105,7 @@ export default async function Home() {
             }
             large={large}
           />
-        ))}
+        ))} */}
       </div>
     </>
   );
